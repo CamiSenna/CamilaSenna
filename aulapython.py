e05c65cdb2ede3d1e0918067f0a465bc57eb1303
CamiSenna <camila_senna@live.com>
2022-08-29 23:50:51 -0300

    exerciciopython

---
+++ aulapython.py
@@ -0,0 +1,19 @@
+# from functools import reduce
+
+# lista = [1,2,3,4]
+# soma = reduce(lambda x, y: x+y, lista, 5)
+# print (soma)
+
+def foo(value):
+    print(f'Olá, esse é o meu parametro: {value}')
+
+foo('LuizaCode')
+
+
+# def ExercicioExtra(meu_dia):
+#   print(f'Hoje é o meu {meu_dia}')
+  
+# ExercicioExtra ('aniversário!')
+
+
+    
