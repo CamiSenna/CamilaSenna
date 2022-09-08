4a10957ab6eca4c3525b524225643a55f506f01e
CamiSenna <camila_senna@live.com>
2022-09-08 00:37:30 -0300

    ajuste

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# CamilaSenna
-Meu reposítório - Aula Git 27/08
+# Camila Senna
 
-Iniciando meu arquivo readme
+ <img src="fotoeu.jpeg?w=512">
 
 ## Um pouco mais sobre a Camila
 - Carioca, morando em Porto Alegre um pouco mais de quatro anos.
 - Estudante de Engenharia da Computação.
 - Sonho em trabalhar com sistemas embarcados.
 - Atualmente trabalho como Analista de Suporte na LinxCommerce
+
+> _"O importante é não parar de questionar; a curiosidade tem sua própria razão de existir."_
--- aulapython.py
+++ aulapython.py
@@ -3,27 +3,41 @@
 lista = [1,2,3,4]
 soma = reduce(lambda x, y: x+y, lista, 5)
 print (soma)
-
+###
 def foo(value):
     print(f'Olá, esse é o meu parametro: {value}')
 
 foo('LuizaCode')
 
-
+###
 def ExercicioExtra(meu_dia):
    print(f'Hoje é o meu {meu_dia}')
   
 ExercicioExtra ('aniversário!')
 
-
+###
 def ExercicioExtra(minha_data, meu_dia):
    print(f'Hoje, dia {minha_data} é o meu {meu_dia}!')
   
-ExercicioExtra ('30 de agosto','aniversário')'''
-
+ExercicioExtra ('30 de agosto','aniversário')
+###
 valor_energia = float(input("Informe o valor da conta de energia."))
 qtd_moradores = input("Quantos moradores:")
 qtd_moradores = int(qtd_moradores)
 valor_por_morador = valor_energia / qtd_moradores
-print((valor_por_morador))
+print((valor_por_morador))'''
+###
+lista = ['Banana','Caju','Melao', 5, 'Pera',10]
+for index, elemento in enumerate(lista): 
+    if elemento == int;
+    
 
+#tentar utilizar a sublista aqui
+#pesquisar decorator, utilizar decorator
+
+#fazer itens do carrinho
+
+
+
+
+    
