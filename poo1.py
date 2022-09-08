197eb2a88237b797818c35db920703e29a1356bf
CamiSenna <camila_senna@live.com>
2022-09-07 23:32:24 -0300

    removendocomentariosbase

--- poo1.py
+++ poo1.py
@@ -35,8 +35,7 @@ print(jogador2.get_classe())
 print(jogador2.get_raca())
 
 
-        # segundo_jogador = Personagem ('Ladino', 'Pequenes', 'Doppo')
-    
+  
         # return f"{jogador1.nome} é da classe {jogador1.classe} e tem a raca{jogador1.raca}."
         # print(f'{segundo_jogador.nome} é da classe {segundo_jogador.classe} e tem a raca{segundo_jogador.raca}.')
     
@@ -48,24 +47,3 @@ print(jogador2.get_raca())
   
        
         
-    
-# class HogwartsHouse:
-#     def __init__(self, name, color, founder):
-#         self.name = name
-#         self.color = color
-#         self.founder = founder
-
-#     def get_points_house(self):
-#         return f"A casa {self.name} possui a pontuação total \
-#                 de 50 pontos"
-
-#     def play_quidditch(self):
-#         return f"A pontuação do quadribol foi de 10 pontos"
-
-
-# hufflepuff = HogwartsHouse('Lufa-Lufa',
-#                            'Amarelo e Preto',
-#                            'Helga Lufa-Lufa')
-
-# points_house = hufflepuff.get_points_house()
-# print('Pontuação =>', points_house)
